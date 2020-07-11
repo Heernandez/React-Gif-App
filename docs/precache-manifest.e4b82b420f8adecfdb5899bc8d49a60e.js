37ec3a64f9524c9be680dcbc2d4b27bb043c2c2a
Heernandez <luishernandez@utp.edu.co>
2020-07-11 11:10:16 -0500

    docs agregado

---
+++ docs/precache-manifest.e4b82b420f8adecfdb5899bc8d49a60e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a06cf666a443c0ba57f2858a690e5a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a503f27c43ebd63484ff",
+    "url": "/static/css/main.5f2c219e.chunk.css"
+  },
+  {
+    "revision": "2c7275a2411ef8944e9b",
+    "url": "/static/js/2.5163bc94.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5163bc94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a503f27c43ebd63484ff",
+    "url": "/static/js/main.d95b9e91.chunk.js"
+  },
+  {
+    "revision": "4d3c5aad2ee9da455662",
+    "url": "/static/js/runtime-main.a4f11275.js"
+  }
+]);
